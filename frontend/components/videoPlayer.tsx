3abebf6ce008a88c5570aad9a945954464d2b78c
isaias-silva <zackblackmangold@gmail.com>
2023-02-16 21:52:30 -0300

    funcionando porém oh my god

--- frontend/components/videoPlayer.tsx
+++ frontend/components/videoPlayer.tsx
@@ -1,26 +1,36 @@
 import { useEffect, useRef, useState } from "react";
 import videojs from "video.js";
+import Player from "video.js/dist/types/player";
 import "video.js/dist/video-js.css";
 
 function VideoPlay() {
-  const play = {
-    fill: true,
-    fluid: true,
-    autoplay: true,
-    controls: true,
-    preload: "none",
-    sources: [
-      {
-        src: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8",
-        type: "application/x-mpegURL",
-      }
-    ],
-    liveui: true,
-  };
   const videoNode = useRef(null);
-  const [player, setPlayer] = useState(null);
+  const [player, setPlayer] = useState<Player>(null);
 
   useEffect(() => {
+    const play = {
+      fill: true,
+      fluid: true,
+      autoplay: true,
+      controls: true,
+      preload: "auto",
+      sources: [
+        {
+          src: " http://xyz.lattv.com.co:8080/playlist/Joao8095/Joao8095/m3u?output=rtmp",
+          type: "application/x-mpegURL",
+        },
+        {
+          src: "http://xyz.lattv.com.co:8080/playlist/Joao8095/Joao8095/m3u?output=hls",
+          type: "application/x-mpegURL",
+        },
+        {
+          src: "   http://xyz.lattv.com.co:8080/playlist/Joao8095/Joao8095/m3u_plus",
+          type: "application/x-mpegURL",
+        },
+      ],
+      liveui: true,
+    };
+
     if (videoNode.current) {
       const _player = videojs(videoNode.current, play, () =>
         console.log("Video iniciado")
@@ -32,7 +42,7 @@ function VideoPlay() {
         }
       };
     }
-  }, [setPlayer]);
+  }, [player, setPlayer]);
 
   return (
     <div data-vjs-player>
