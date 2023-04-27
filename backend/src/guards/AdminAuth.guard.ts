13779e3c16d46e5b0fe79dd49e83e5045d7a306b
Joao Calado <jcaladomoura@gmail.com>
2023-04-26 23:05:26 -0300

    via

--- backend/src/guards/AdminAuth.guard.ts
+++ backend/src/guards/AdminAuth.guard.ts
@@ -1,13 +1,28 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Observable } from 'rxjs';
-
+import { ExtractJwt } from 'passport-jwt';
 @Injectable()
 export default class AdminGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log(request)
+    const info = ExtractJwt.fromExtractors([
+      (request: any) => {
+        
+        const cookie: string = request?.headers?.cookie;
+       
+        if(!cookie){
+          throw new UnauthorizedException("No has hecho login");
+        }
+  
+        const token = /Authentication=([^;]+)/.exec(cookie)
+ 
+
+        return token[1]
+      },
+    ])
+    console.log(info)
     const user = request.user;
     console.log("opa adm")
     console.log(user)
