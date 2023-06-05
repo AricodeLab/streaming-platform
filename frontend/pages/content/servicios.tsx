2c7ab88304611293c1ec11026fee861ff365fe9c
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-06-05 18:49:04 -0300

    add plazo in servicios

--- frontend/pages/content/servicios.tsx
+++ frontend/pages/content/servicios.tsx
@@ -1,17 +1,24 @@
-import { useEffect,useContext } from "react";
 import Layout from "../../components/layout";
-import { AuthContext } from "../../context/AuthContext";
 import VideoPlay from "../../components/videoPlayer";
 import {recoverUserInfo} from "../../service/api"
 import { GetServerSideProps } from 'next'
 import { parseCookies } from 'nookies'
 
-export  default function Web() {
-  
+export  default function Web({info}) {
+  const currentDate = new Date();
+  const plazoDate = new Date(info.plazo);
+
+  const timeDifference = plazoDate.getTime() - currentDate.getTime();
+  const daysLeft = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hoursLeft = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutesLeft = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const secondsLeft = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
   return (
     <Layout>
       <div className="flex flex-col text-white w-full h-full items-center">
-        
+       <h1>Plazo: {info.plazo}</h1>
+        <h2>Time Left: {daysLeft} days, {hoursLeft} hours, {minutesLeft} minutes, {secondsLeft} seconds</h2>
         <VideoPlay id={1} title="title" streaming_url="https://raw.githubusercontent.com/Alextremo123/lista-m3u/main/ALEX"/>
       </div>
     </Layout>
@@ -33,6 +40,11 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   try{
     const info = await recoverUserInfo(cokkies)
+    return {
+      props: {
+        info
+      },
+    };
   } catch(e){
     console.log(e)
     return {
@@ -43,9 +55,4 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     }
   }
   
-
-
-  return {
-    props: {}
-  }
 }
